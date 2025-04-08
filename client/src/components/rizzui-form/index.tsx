2023adb0272c2b62ec694de56bdbffc8c0a90d74
Nachman-libermensh <bn8475266@gmail.com>
2025-04-08 14:44:19 +0300

    feat: refactor form components

--- client/src/components/rizzui-form/index.tsx
+++ client/src/components/rizzui-form/index.tsx
@@ -1,15 +1,12 @@
 import { createFormHook, createFormHookContexts } from "@tanstack/react-form";
-import TextField from "./form-components/text-field";
+import TextField from "./form-components/text-field.field";
 import CheckboxField from "./form-components/checkbox-field";
 import { SubmitButton } from "./form-components/submit-button";
-import { PasswordInput } from "./form-components/password-input";
-import { SelectLookupField } from "./form-components/select-field";
-import { SelectUserField } from "./form-components/select-user-field";
+import { PasswordInput } from "./form-components/password-input.field";
 import TextAreaField from "./form-components/text-area-field";
 import FormLayout from "./form-components/form-layout";
 import { FormSection } from "./form-components/FormSection";
-import DatePickerField from "./form-components/date-picker";
-import SelectMultiLookupField from "./form-components/select-multi-lookup.field";
+import DatePickerField from "./form-components/date-picker.field";
 import FileUploadField from "./form-components/file-upload.field";
 
 // יצירת Context עבור השדות והטופס
@@ -22,9 +19,7 @@ export const { useAppForm, withForm } = createFormHook({
   fieldComponents: {
     TextField,
     CheckboxField,
-    SelectLookupField,
-    SelectMultiLookupField,
-    SelectUserField,
+
     PasswordInput,
     TextAreaField,
     DatePickerField,
