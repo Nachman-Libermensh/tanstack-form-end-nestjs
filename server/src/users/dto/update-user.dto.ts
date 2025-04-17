f80bdaa6ed9eddf7e78e261cb92d665d97d06758
Nachman-libermensh <bn8475266@gmail.com>
2025-04-17 21:23:27 +0300

    Changing the folder strategy in the project, removing the shared & server folder, to focus exclusively on the project's goal of serving as a form builder and an indicator of an effective form strategy

--- server/src/users/dto/update-user.dto.ts
+++
@@ -1,7 +0,0 @@
-// import { PartialType } from '@nestjs/mapped-types';
-import { PartialType } from '@nestjs/swagger'; // ייבוא PartialType מ-NestJS Swagger
-
-import { CreateUserDto } from './create-user.dto';
-
-// הרחבה של CreateUserDto עם כל השדות אופציונליים
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
